5351041ee25629212b8848636e6a8df59ef40409
dan5 <dan5.ya@gmail.com>
2013-09-19 14:37:43 +0900

    rails new ham0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ham0::Application.config.secret_key_base = 'bfe47f95342c4be30e37ef7bc8731b1c1dba739b75227fc6c0b103306c1b379d28699e326c3659f5ff23a0ce74b7f0c2764eca9bcc00061edb6801275d5297bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ham0::Application.config.session_store :cookie_store, key: '_ham0_session'
