619a316f07ebf3464965a0a3f1f0b9ed97a01d75
dan5 <dan5.ya@gmail.com>
2013-09-19 14:49:15 +0900

    hamlに切り替えた

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Ham0::Application.routes.draw do
+  get "game/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
